eb0193f73dcda7e533b2e5ba223f6c500565d8eb
Armandosamarbide <martin.salduna@gmail.com>
2023-05-16 11:37:42 -0300

    Contenidos 1505

---
+++ Clase15052023/script.js
@@ -0,0 +1,50 @@
+/*
+
+Javascript es un lenguaje de programación interpretado, de alto nivel, debilmente tipado.
+No hay un momento de compilación, es "en el momento"
+
+*/
+
+console.log('Hola')
+
+// TIPOS DE DATOS PRIMITIVOS
+
+// Numbers: dato numérico. Ex: 90, -90, 5.4, etc.
+// Los datos numéricos NO van entre comillas.
+
+// NO hay decimales en Javascript, hay FLOTANTES
+// Infinity
+// NaN = Not a Number
+
+// Strings: cadena de texto.
+// Va entre comillas, pueden ser '' o "". Ex: 'Menganito es un buen alumno'.
+
+// Booleans
+// Tipo de dato que puede ser verdadero o falso.
+// Se escribe true o false, no hay otro.
+
+// Undefined
+// Es "undefined". Va a aparecer cuando un dato no esté definido. Ex: cuándo terminamos el curso es "undefined",
+
+// NULL
+// NULL es "ausencia de valor".
+
+// Symbol (No es parte de la cursada, no lo vamos a ver)
+
+// OPERADORES ARITMÉTICOS
+
+// + - * / %, suma, resta, multiplicación, división, resto de la división.
+// Siempre devuelven un tipo de dato numérico.
+// typeof <argumento>: Devuelve el tipo de dato.
+
+// +
+
+// Si sumo dos strings, los concatena.
+
+// COMPARADORES
+// Necesita dos datos para funcionar, siempre devuelven boolean.
+
+// ==, ===, !=, !==, >, <, =<
+
+// ==: para comparar igualdad.
+
